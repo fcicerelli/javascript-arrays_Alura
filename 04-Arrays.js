1bead412b6e84e3c9496fe66c67f129b01defc8c
fcicerelli <fcicerellitic@gmail.com>
2025-03-21 19:24:46 -0300

    arrays

---
+++ 04-Arrays.js
@@ -0,0 +1,20 @@
+let arrayVacio = [];
+
+console.log(arrayVacio)
+console.log(arrayVacio.length)
+
+console.log();
+
+arrayVacio= [,,,];
+console.log(arrayVacio.length)
+console.log(arrayVacio[0])
+console.log(arrayVacio[1])
+console.log(arrayVacio[2])
+
+console.log();
+
+arrayVacio = [,,,];
+console.log(arrayVacio.length);
+arrayVacio.push(50);
+console.log(arrayVacio);
+console.log(arrayVacio.length);
