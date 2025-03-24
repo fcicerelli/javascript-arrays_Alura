81bc6e42bee4d189d85bb491ff597f8a108bbf58
fcicerelli <fcicerellitic@gmail.com>
2025-03-24 20:25:01 -0300

    splice

---
+++ 06-Splice.js
@@ -0,0 +1,6 @@
+const lstAlumnos = ['Lucía','Martina','Hugo','Daniel',
+                    'Martín','María']; 
+
+lstAlumnos.splice(2,2,'Diego','José','Leonardo'); // Elimina a Hugo y a Daniel
+                                                  // y agrega a los nuevos en su lugar
+console.log(lstAlumnos);
